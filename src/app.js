9071b843f352487216995d15b1f6abf68242c592
Cesar Bahena <cabahenac@gmail.com>
2022-03-13 08:09:13 -0600

    Refactored getTotals to function calculate that accepts several operations

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import {load} from 'cheerio';
 import { post, getCookie } from './inventory/application/requests.js';
 import { getConsumos, filterData } from './consumos/application/parsers.js';
 import { classifyConsumos } from './consumos/application/classify-consumos.js';
-import { getTotals } from './consumos/application/calculations.js';
+import { calculate, getTotal, getPx } from './consumos/application/calculations.js';
 import { domain, reqConfig } from './inventory/domain/config.js';
 import { getRows, getInventory } from './inventory/application/parsers.js';
 import { enoughInventory } from './shared/application/calculations.js';
@@ -32,8 +32,8 @@ const searchParams = {
   
   let consumos = await getConsumos('03/03/2023', '03/08/2023');
   consumos = filterData(consumos);
-  consumos = classifyConsumos(consumos, ['px', 'rep', 'qc', 'man']);
-  consumos = getTotals(consumos);
+  consumos = classifyConsumos(consumos, ['res', 'rep', 'qc', 'man']);
+  consumos = calculate(consumos, [getTotal, getPx]);
   
   const [toCapture, toLog] = enoughInventory(inventory, consumos);
   console.log(toCapture);
--- src/consumos/application/calculations.js
+++ src/consumos/application/calculations.js
@@ -1,11 +1,23 @@
 
 
-export function getTotals(consumos) {
+export function getTotal(consumos, rvo) {
+  consumos[rvo].total = 0;
+  for (let col of ['res', 'rep', 'qc', 'man']) {
+    consumos[rvo].total += consumos[rvo][col];
+  }
+}
+
+
+export function getPx(consumos, rvo) {
+  consumos[rvo].px = consumos[rvo].res - consumos[rvo].man;
+}
+
+
+export function calculate(consumos, operations) {
   for (let rvo in consumos) {
-    consumos[rvo].total = 0;
-    for (let col in consumos[rvo]) {
-      consumos[rvo].total += consumos[rvo][col];
+    for (let operation of operations) {
+      operation(consumos, rvo);
     }
   }
-  return consumos;
+  return consumos
 }
